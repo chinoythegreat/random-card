3fc4b99a6af6b1c7762661794768657a1878baf5
Chinoy Aller <chinoy316@gmail.com>
2016-02-17 23:28:22 -0800

    Added populating of card number and car d suit flippers. Added card saving to Realm database. Added slot animation.

--- app/src/main/java/sample/chinoy/randomcardgenerator/MainActivity.java
+++ app/src/main/java/sample/chinoy/randomcardgenerator/MainActivity.java
@@ -1,15 +1,43 @@
 package sample.chinoy.randomcardgenerator;
 
+import android.content.Context;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.os.Handler;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.Animation;
+import android.view.animation.TranslateAnimation;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ViewFlipper;
+
+import java.util.Random;
+
+import io.realm.Realm;
+import io.realm.RealmResults;
+
+// Slot machine animation
+// http://www.techrepublic.com/blog/software-engineer/building-a-slot-machine-in-android-viewflipper-meet-gesture-detector/
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int mSpeed = 90;
+
+    private int mCount;
+    private int mCardNumber;
+    private int mCardSuit;
+    private Realm mCardDatabase;
+    private String[] mSuitStrings;
+    private ViewFlipper mCardViewFlipper;
+    private ViewFlipper mNumberViewFlipper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,14 +46,118 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+        populateCardNumberFlipper();
+        populateCardSuitFlipper();
+        mSuitStrings = getResources().getStringArray(R.array.cards_array);
+
+        mCardDatabase = Realm.getInstance(MainActivity.this);
+    }
+
+    private void populateCardNumberFlipper() {
+        mNumberViewFlipper = (ViewFlipper) findViewById(R.id.number_view_flipper);
+        String[] cardNumberStrings = getResources().getStringArray(R.array.card_number_array);
+        for(int i = 0; i < cardNumberStrings.length; i++) {
+            LayoutInflater inflater = (LayoutInflater) this
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View view = inflater.inflate(R.layout.card_number, null);
+
+            mNumberViewFlipper.addView(view);
+            TextView numberText = (TextView) view.findViewById(R.id.card_number);
+            numberText.setText(cardNumberStrings[i]);
+        }
+    }
+
+    private void populateCardSuitFlipper() {
+        mCardViewFlipper = (ViewFlipper) findViewById(R.id.suit_view_flipper);
+        int cardSuitInt[] = {
+                R.drawable.suit_club,
+                R.drawable.suit_diamond,
+                R.drawable.suit_heart,
+                R.drawable.suit_spade};
+        for(int i = 0; i < cardSuitInt.length; i++) {
+            LayoutInflater inflater = (LayoutInflater) this
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View view = inflater.inflate(R.layout.card_suit, null);
+
+            mCardViewFlipper.addView(view);
+            ImageView suitImage = (ImageView) view.findViewById(R.id.card_suit);
+            suitImage.setImageDrawable(ContextCompat.getDrawable(MainActivity.this, cardSuitInt[i]));
+        }
+    }
+
+    public void onClickLayout(final View view) {
+        mCardNumber = new Random().nextInt(14 - 1) + 1;
+        mCardSuit = new Random().nextInt(5-1) + 1;
+        mCount = 20;
+
+        Handler h = new Handler();
+        h.postDelayed(slotMachineRunnable, mSpeed);
+
+        saveGeneratedCardToDatabase(mCardNumber, mCardSuit);
+        Toast.makeText(MainActivity.this, mCardNumber + " of " + mSuitStrings[mCardSuit -1], Toast.LENGTH_SHORT).show();
+
+        logAllGeneratedCards();
+    }
+
+    private void saveGeneratedCardToDatabase(int cardNumber, int cardSuit) {
+        mCardDatabase.beginTransaction();
+        Card card = mCardDatabase.createObject(Card.class);
+        card.setCardNumber(cardNumber);
+        card.setCardSuit(cardSuit);
+        mCardDatabase.commitTransaction();
+    }
+
+    private void logAllGeneratedCards() {
+        RealmResults<Card> cardResults =
+                mCardDatabase.where(Card.class).findAll();
+        Log.d("card", "num of cards: " + cardResults.size());
+
+        for(Card card:cardResults) {
+            Log.d("card", card.getcardNumber() + " of " + card.getCardSuit());
+        }
+    }
+
+    private Runnable slotMachineRunnable = new Runnable() {
+
+        @Override
+        public void run() {
+            upAnimation(mCardViewFlipper);
+            upAnimation(mNumberViewFlipper);
+            if (mCount>1) {
+                Handler h = new Handler();
+                h.postDelayed(slotMachineRunnable, mSpeed);
+            } else {
+                mCardViewFlipper.setDisplayedChild(mCardSuit -1);
+                mNumberViewFlipper.setDisplayedChild(mCardNumber -1);
             }
-        });
+        }
+
+    };
+
+    private void upAnimation(final ViewFlipper viewFlipper) {
+        mCount--;
+        Animation outToBottom = new TranslateAnimation(
+                Animation.RELATIVE_TO_PARENT, 0.0f,
+                Animation.RELATIVE_TO_PARENT, 0.0f,
+                Animation.RELATIVE_TO_PARENT, 0.0f,
+                Animation.RELATIVE_TO_PARENT, 1.0f);
+        outToBottom.setInterpolator(new AccelerateInterpolator());
+        outToBottom.setDuration(mSpeed);
+        Animation inFromTop = new TranslateAnimation(
+                Animation.RELATIVE_TO_PARENT, 0.0f,
+                Animation.RELATIVE_TO_PARENT, 0.0f,
+                Animation.RELATIVE_TO_PARENT, -1.0f,
+                Animation.RELATIVE_TO_PARENT, 0.0f);
+        inFromTop.setInterpolator(new AccelerateInterpolator());
+        inFromTop.setDuration(mSpeed);
+        viewFlipper.clearAnimation();
+        viewFlipper.setInAnimation(inFromTop);
+        viewFlipper.setOutAnimation(outToBottom);
+        if (viewFlipper.getDisplayedChild()==0) {
+            viewFlipper.setDisplayedChild(viewFlipper.getChildCount() - 1);
+        } else {
+            viewFlipper.showNext();
+        }
     }
 
     @Override
